8984545173e54ab6fda3107e3cb07036a930b737
Max WFY <maxwebfactory@gmail.com>
2017-02-28 18:01:41 +0200

    adding class PostManager to set up view permissions

--- posts/models.py
+++ posts/models.py
@@ -2,11 +2,16 @@ from django.conf import settings
 from django.core.urlresolvers import reverse
 from django.db import models
 from django.db.models.signals import pre_save
+from django.utils import timezone
 
 from django.utils.text import slugify
 # Create your models here.
 # MVC MODEL VIEW CONTROLLER
 
+
+class PostManager(models.Manager):
+	def active(self, *args, **kwargs):
+		return super(PostManager, self).filter(draft=False).filter(publish__lte=timezone.now())
 
 def upload_location(instance, filename):
     #filebase, extension = filename.split(".")
@@ -25,8 +30,12 @@ class Post(models.Model):
 	height_field = models.IntegerField(default=0)
 	width_field = models.IntegerField(default=0)
 	content = models.TextField()
+	draft = models.BooleanField(default=False)
+	publish = models.DateField(auto_now=False, auto_now_add=False)
 	updated = models.DateTimeField(auto_now=True, auto_now_add=False)
 	timestamp = models.DateTimeField(auto_now=False, auto_now_add=True)
+
+	objects = PostManager()
 
 	def __unicode__(self):
 		return self.title
