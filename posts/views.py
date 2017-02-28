1956cf880e7e9dfae1e3de968b5abb9ada4e670f
Max WFY <maxwebfactory@gmail.com>
2017-02-28 09:40:47 +0200

    adding image uploads

--- posts/views.py
+++ posts/views.py
@@ -1,4 +1,5 @@
 from django.contrib import messages
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render, get_object_or_404, redirect
 
@@ -9,7 +10,7 @@ from .models import Post
 
 
 def post_create(request):
-	form = PostForm(request.POST or None)
+	form = PostForm(request.POST or None, request.FILES or None)
 	if form.is_valid():
 		instance = form.save(commit=False)
 		instance.save()
@@ -29,16 +30,30 @@ def post_detail(request, id=None):
 	return render(request, "post_detail.html", context)
 
 def post_list(request):
-	queryset = Post.objects.all()
+	queryset_list = Post.objects.all() #.order_by("-timestamp"), in this app this works via Meta class->ordering in models.py
+	paginator = Paginator(queryset_list, 10) # Show 25 queryset per page
+	page_request_var = "page"
+	page = request.GET.get(page_request_var)
+	try:
+		queryset = paginator.page(page)
+	except PageNotAnInteger:
+		# If page is not an integer, deliver first page.
+		queryset = paginator.page(1)
+	except EmptyPage:
+		# If page is out of range (e.g. 9999), deliver last page of results.
+		queryset = paginator.page(paginator.num_pages)
+
 	context = {
 		"object_list": queryset,
-		"title": "List"
+		"title": "List",
+		"page_request_var": page_request_var 
 	}
 	return render(request, "post_list.html", context)
 
+
 def post_update(request, id=None):
 	instance = get_object_or_404(Post, id=id)
-	form = PostForm(request.POST or None, instance = instance)
+	form = PostForm(request.POST or None, request.FILES or None, instance = instance)
 	if form.is_valid():
 		instance = form.save(commit=False)
 		instance.save()
